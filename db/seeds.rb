0485457496ef0272be78385615e407e3a04b88ff
Alexandre PETIT <alexandre.petit.vilapon@gmail.com>
2023-04-15 17:43:36 +0200

    Add Product controller, model and its fixtures

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Product.create(title: "A flight ticket to Bordeaux", description: "A premium ticket to fly back and forth from Paris to Bordeaux", image_url: "https://www.air-journal.fr/wp-content/uploads/air-journal-air-france-airbus-a350-source-com-AF-1.jpg", quantity: 1, price: 299.99)
+Product.create(title: "Laptop", description: "A powerful laptop to work everywhere", image_url: "https://files.refurbed.com/pi/hp-probook-650-g4-8350u-1625638707.jpg", quantity: 20, price: 1499.99)
+Product.create(title: "Headset", description: "A wireless headset with good sound quality", image_url: "https://resource.logitech.com/content/dam/gaming/en/products/pro-wireless/pro-wireless-headset-gallery-1.png", quantity: 5, price: 149.99)
