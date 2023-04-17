58e3e4ca3b4cf397292e5d76f3f8272660251377
Alexandre PETIT <alexandre.petit.vilapon@gmail.com>
2023-04-17 22:32:46 +0200

    Fix database constraint to allow null value for user_id in Carts so that user can have a cart without account

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_16_154517) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_17_203129) do
   create_table "cart_items", force: :cascade do |t|
     t.integer "cart_id", null: false
     t.integer "product_id", null: false
@@ -22,7 +22,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_16_154517) do
   end
 
   create_table "carts", force: :cascade do |t|
-    t.integer "user_id", null: false
+    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_carts_on_user_id"
